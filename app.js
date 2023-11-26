6a55458ee2730e34d0e99674c80424ea8c523f19
alameen-alli <allialameen8739@gmail.com>
2023-11-26 20:45:02 +0100

    fix

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ const MENUID = 1;
 const NOTIFICATIONS_BTN = document.getElementById("notification-btn");
 const NOTIFICATIONS_CONTAINER = document.getElementById("notifications");
 const ARIA_NOTIFICATIONS = document.getElementById("alerts-notify");
-const POPUPS = [NOTIFICATIONS_CONTAINER, MENU];
-const POPUP_BTNS = [NOTIFICATIONS_BTN, PROFILE_MENU_BTN];
+const DROPDOWN = [NOTIFICATIONS_CONTAINER, MENU];
+const DROPDOWN_BTNS = [NOTIFICATIONS_BTN, PROFILE_MENU_BTN];
 const NOTIFICATIONID = 0;
-const POPUPS_ARIA_NOTIFICATION = [ARIA_NOTIFICATIONS, PROFILE_MENU_ARIA_NOTIFICATION];
+const DROPDOWN_ARIA_NOTIFICATION = [ARIA_NOTIFICATIONS, PROFILE_MENU_ARIA_NOTIFICATION];
 const TRIAL_CALLOUT = document.getElementById("select-a-plan-callout");
 const TRIAL_CALLOUT_CLOSE_BTN = document.getElementById(
   "trial-callout-close-button"
@@ -22,62 +22,62 @@ const SETUP = document.getElementById("setup");
 const TOGGLE_SETUP_ARIA_NOTIFICATION = document.getElementById(
   "toggle-setup-notify"
 );
-const PROGRESSBAR = document.getElementById("setupguide-progess-bar");
+const SETUPGUIDE_PROGRESSBAR = document.getElementById("setupguide-progess-bar");
 const PROGRESS_COUNT = document.getElementById("setupguide-progress-count");
-const TOGGLE_SETUP_STEP_VISIBILITY_BTNS =
+const SETUPGUIDE_STEP_VISIBILITY_TOGGLE_BTNS =
   document.querySelectorAll(".setup-step-toggle");
-const SETUP_STEPS = [...document.querySelectorAll(".setup-step")];
+const SETUPGUIDE_STEPS = [...document.querySelectorAll(".setup-step")];
 const ACTIVE_CLASS = "active";
-const SETUP_STEPS_ARIA_NOTIFICATIONS =
+const SETUPGUIDE_STEPS_ARIA_NOTIFICATIONS =
   document.querySelectorAll(".setup-step-notify");
-const TOGGLE_SETUP_STEP_COMPLETE_BTNS =
+const SETUPGUIDE_STEPS_COMPLETE_BTNS =
   document.querySelectorAll(".check-step-btn");
-const TOGGLE_COMPLETE_ARIA_NOTIFICATIONS = document.querySelectorAll(
+const TOGGLE_ARIA_NOTIFICATIONS_ON_COMPLETE = document.querySelectorAll(
   ".check-step-btn-notify"
 );
 
-const HIDE_POPUPS = () => {
-  POPUPS.forEach((popup, index) => {
-    const IS_POPUP_OPEN = !popup.classList.contains(HIDDENSTYLE);
-    if (!IS_POPUP_OPEN) {
+const HIDE_DROPDOWN = () => {
+  DROPDOWN.forEach((popup, index) => {
+    const IS_DROPDOWN = !popup.classList.contains(HIDDENSTYLE);
+    if (!IS_DROPDOWN) {
       return;
     }
 
     popup.classList.add(HIDDENSTYLE);
-    POPUP_BTNS[index].setAttribute("aria-expanded", false);
-    const POPUP_ARIA_NOTIFICATION = POPUPS_ARIA_NOTIFICATION[index];
-    POPUP_ARIA_NOTIFICATION.setAttribute(
+    DROPDOWN_BTNS[index].setAttribute("aria-expanded", false);
+    const DROPDOWN_ARIA = DROPDOWN_ARIA_NOTIFICATION[index];
+    DROPDOWN_ARIA.setAttribute(
       "aria-label",
-      POPUP_ARIA_NOTIFICATION.dataset.closeLabel
+      DROPDOWN_ARIA.dataset.closeLabel
     );
   });
 };
 
 
-const TOGGLE_POPUP = (event, POPUP_INDEX) => {
-  const POPUP = POPUPS[POPUP_INDEX];
-  const POPUP_BTN = POPUP_BTNS[POPUP_INDEX];
-  const IS_POPUP_OPEN = !POPUP.classList.contains(HIDDENSTYLE);
+const TOGGLE_POPUP = (event, DROPDOWNID) => {
+  const POPUP = DROPDOWN[DROPDOWNID];
+  const POPUP_BTN = DROPDOWN_BTNS[DROPDOWNID];
+  const IS_DROPDOWN = !POPUP.classList.contains(HIDDENSTYLE);
 
-  HIDE_POPUPS();
+  HIDE_DROPDOWN();
 
-  if (!IS_POPUP_OPEN) {
+  if (!IS_DROPDOWN) {
     POPUP.classList.remove(HIDDENSTYLE);
     POPUP_BTN.setAttribute("aria-expanded", true);
-    const POPUP_ARIA_NOTIFICATION = POPUPS_ARIA_NOTIFICATION[POPUP_INDEX];
-    POPUP_ARIA_NOTIFICATION.setAttribute(
+    const DROPDOWN_ARIA = DROPDOWN_ARIA_NOTIFICATION[DROPDOWNID];
+    DROPDOWN_ARIA.setAttribute(
       "aria-label",
-      POPUP_ARIA_NOTIFICATION.dataset.openLabel
+      DROPDOWN_ARIA.dataset.openLabel
     );
     event.stopPropagation();
   }
 };
 
 
-const HIDE_POPUPS_ON_CLICK_OUTSIDE = (event) => {
-  const IS_ANY_POPUP_CLICKED = POPUPS.some((popup) => popup.contains(event.target));
+const HIDE_DROPDOWN_ON_CLICK_OUTSIDE = (event) => {
+  const IS_ANY_POPUP_CLICKED = DROPDOWN.some((popup) => popup.contains(event.target));
   if (!IS_ANY_POPUP_CLICKED) {
-    HIDE_POPUPS();
+    HIDE_DROPDOWN();
   }
 };
 
@@ -85,7 +85,7 @@ const FOCUS_FIRST_MENU_ITEM = () => MENU_ITEMS.item(0).focus();
 
 const HANDLE_ESCAPE_KEY_PRESS = (event) => {
   if (event.key === "Escape") {
-    HIDE_POPUPS();
+    HIDE_DROPDOWN();
   }
 };
 
@@ -121,47 +121,47 @@ const TOGGLE_SETUP = () => {
 };
 
 const SHOW_SETUP_STEP = (SETUP_STEP_INDEX) => {
-  HIDE_SETUP_STEPS();
-  SETUP_STEPS[SETUP_STEP_INDEX].classList.add(ACTIVE_CLASS);
+  HIDE_SETUPGUIDE_STEPS();
+  SETUPGUIDE_STEPS[SETUP_STEP_INDEX].classList.add(ACTIVE_CLASS);
 
   const SETUP_STEP_ARIA_NOTIFICATION =
-    SETUP_STEPS_ARIA_NOTIFICATIONS.item(SETUP_STEP_INDEX);
+    SETUPGUIDE_STEPS_ARIA_NOTIFICATIONS.item(SETUP_STEP_INDEX);
   SETUP_STEP_ARIA_NOTIFICATION.setAttribute(
     "aria-label",
     `Setup step ${SETUP_STEP_INDEX + 1} opened`
   );
 
-  TOGGLE_SETUP_STEP_VISIBILITY_BTNS.item(SETUP_STEP_INDEX).setAttribute(
+  SETUPGUIDE_STEP_VISIBILITY_TOGGLE_BTNS.item(SETUP_STEP_INDEX).setAttribute(
     "aria-expanded",
     true
   );
 };
 
-const HIDE_SETUP_STEPS = () => {
-  SETUP_STEPS.forEach((el, INDEX) => {
+const HIDE_SETUPGUIDE_STEPS = () => {
+  SETUPGUIDE_STEPS.forEach((el, INDEX) => {
     const IS_CLOSED = !el.classList.contains(ACTIVE_CLASS);
     if (IS_CLOSED) {
       return;
     }
 
     el.classList.remove(ACTIVE_CLASS);
-    SETUP_STEPS_ARIA_NOTIFICATIONS.item(INDEX).setAttribute(
+    SETUPGUIDE_STEPS_ARIA_NOTIFICATIONS.item(INDEX).setAttribute(
       "aria-label",
       `Setup step ${INDEX + 1} closed`
     );
   });
 
-  TOGGLE_SETUP_STEP_VISIBILITY_BTNS.forEach((btn) =>
+  SETUPGUIDE_STEP_VISIBILITY_TOGGLE_BTNS.forEach((btn) =>
     btn.setAttribute("aria-expanded", false)
   );
 };
 
 const UPDATE_ARIA_FOR_TOGGLE_COMPLETE_BTN = (TOGGLE_BTN_INDEX) => {
-  const SETUP_STEP = SETUP_STEPS[TOGGLE_BTN_INDEX];
+  const SETUP_STEP = SETUPGUIDE_STEPS[TOGGLE_BTN_INDEX];
   const IS_SETUP_STEP_COMPLETE = !!SETUP_STEP.dataset.isCompleted;
-  const TOGGLE_BTN = TOGGLE_SETUP_STEP_COMPLETE_BTNS[TOGGLE_BTN_INDEX];
+  const TOGGLE_BTN = SETUPGUIDE_STEPS_COMPLETE_BTNS[TOGGLE_BTN_INDEX];
   const TOGGLE_COMPLETE_ARIA_NOTIFICATION =
-    TOGGLE_COMPLETE_ARIA_NOTIFICATIONS.item(TOGGLE_BTN_INDEX);
+    TOGGLE_ARIA_NOTIFICATIONS_ON_COMPLETE.item(TOGGLE_BTN_INDEX);
 
   if (IS_SETUP_STEP_COMPLETE) {
     TOGGLE_BTN.setAttribute("aria-label", "Mark step incomplete");
@@ -179,27 +179,27 @@ const UPDATE_ARIA_FOR_TOGGLE_COMPLETE_BTN = (TOGGLE_BTN_INDEX) => {
 };
 
 const TOGGLE_SETUP_STEP_COMPLETE = (TOGGLE_BTN_INDEX) => {
-  const SETUP_STEP = SETUP_STEPS[TOGGLE_BTN_INDEX];
+  const SETUP_STEP = SETUPGUIDE_STEPS[TOGGLE_BTN_INDEX];
   const IS_SETUP_STEP_COMPLETE = !!SETUP_STEP.dataset.isCompleted;
   SETUP_STEP.dataset.isCompleted = IS_SETUP_STEP_COMPLETE ? "" : true;
 
   UPDATE_PROGRESS_BAR();
   UPDATE_ARIA_FOR_TOGGLE_COMPLETE_BTN(TOGGLE_BTN_INDEX);
 
-  const NEXT_STEP_INDEX = FIND_NEXT_UNCOMPLETED_STEP_INDEX(SETUP_STEPS);
+  const NEXT_STEP_INDEX = FIND_NEXT_UNCOMPLETED_STEP_INDEX(SETUPGUIDE_STEPS);
   if (NEXT_STEP_INDEX !== -1) {
     SHOW_SETUP_STEP(NEXT_STEP_INDEX);
   }
 };
 
-const FIND_NEXT_UNCOMPLETED_STEP_INDEX = (SETUP_STEPS) =>
-  SETUP_STEPS.findIndex((STEP) => !STEP.dataset.isCompleted);
+const FIND_NEXT_UNCOMPLETED_STEP_INDEX = (SETUPGUIDE_STEPS) =>
+  SETUPGUIDE_STEPS.findIndex((STEP) => !STEP.dataset.isCompleted);
 
 const UPDATE_PROGRESS_BAR = () => {
-  const COMPLETED_STEPS_NO = SETUP_STEPS.filter(
+  const COMPLETED_STEPS_NO = SETUPGUIDE_STEPS.filter(
     (STEP) => STEP.dataset.isCompleted
   ).length;
-  PROGRESSBAR.style.width = `${COMPLETED_STEPS_NO * 20}%`;
+  SETUPGUIDE_PROGRESSBAR.style.width = `${COMPLETED_STEPS_NO * 20}%`;
   PROGRESS_COUNT.innerText = COMPLETED_STEPS_NO;
 };
 
@@ -218,7 +218,7 @@ PROFILE_MENU_BTN.addEventListener("click", (EVENT) => {
   }
 });
 
-POPUPS.forEach((POPUP) => {
+DROPDOWN.forEach((POPUP) => {
   POPUP.addEventListener("keyup", HANDLE_ESCAPE_KEY_PRESS);
 });
 NOTIFICATIONS_BTN.addEventListener("keyup", HANDLE_ESCAPE_KEY_PRESS);
@@ -229,7 +229,7 @@ MENU_ITEMS.forEach((MENU_ITEM, INDEX) =>
   )
 );
 
-document.addEventListener("click", HIDE_POPUPS_ON_CLICK_OUTSIDE);
+document.addEventListener("click", HIDE_DROPDOWN_ON_CLICK_OUTSIDE);
 
 TRIAL_CALLOUT_CLOSE_BTN.addEventListener("click", () => {
   TRIAL_CALLOUT.classList.add(HIDDENSTYLE);
@@ -237,10 +237,10 @@ TRIAL_CALLOUT_CLOSE_BTN.addEventListener("click", () => {
 });
 
 SETUPGUIDE_TOGGLE_CHECK.addEventListener("click", TOGGLE_SETUP);
-TOGGLE_SETUP_STEP_VISIBILITY_BTNS.forEach((BTN, BTN_INDEX) => {
+SETUPGUIDE_STEP_VISIBILITY_TOGGLE_BTNS.forEach((BTN, BTN_INDEX) => {
   BTN.addEventListener("click", () => SHOW_SETUP_STEP(BTN_INDEX));
 });
 
-TOGGLE_SETUP_STEP_COMPLETE_BTNS.forEach((BTN, BTN_INDEX) => {
+SETUPGUIDE_STEPS_COMPLETE_BTNS.forEach((BTN, BTN_INDEX) => {
   BTN.addEventListener("click", () => TOGGLE_SETUP_STEP_COMPLETE(BTN_INDEX));
 });
